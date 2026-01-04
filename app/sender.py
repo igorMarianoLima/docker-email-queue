34f12d703434b65e8e4cefa30516e842de124820
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-04 19:58:35 -0300

    Feat: Implement queue and worker services to send emails

--- app/sender.py
+++ app/sender.py
@@ -1,30 +1,51 @@
 import psycopg2
-from bottle import route, run, request
+import redis
+import json
+import time
+from bottle import Bottle, request
 
-DSN = 'dbname=email_sender user=postgres password=postgres host=db'
-SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+class Sender(Bottle):
+    def connect_with_retry(self, dsn, retries=10, delay=2):
+        for i in range(retries):
+            try:
+                return psycopg2.connect(dsn)
+            except psycopg2.OperationalError as e:
+                print(f"DB não pronto, tentativa {i+1}/{retries}")
+                time.sleep(delay)
+        raise Exception("Banco não ficou disponível")
 
-def register_message(subject, message):
-    conn = psycopg2.connect(DSN);
-    cur = conn.cursor()
-    cur.execute(SQL, (subject, message))
-    conn.commit()
-    cur.close()
-    conn.close()
+    def __init__(self):
+        super().__init__()
+        self.route('/', method='POST', callback=self.send)
+        self.queue = redis.StrictRedis(host='queue', port=6379, db=0)
 
-    print('Registered email!')
+        DSN = 'dbname=email_sender user=postgres password=postgres host=db'
+        self.conn = self.connect_with_retry(DSN)
 
-@route('/', method='POST')
-def send():
-    subject = request.forms.get('subject')
-    message = request.forms.get('message')
+    def register_message(self, subject, message):
+        SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
 
-    register_message(subject, message)
+        cur = self.conn.cursor()
+        cur.execute(SQL, (subject, message))
+        self.conn.commit()
+        cur.close()
 
-    return 'Queued message! Subject: {}. Message: {}'.format(
-        subject,
-        message
-    )
+        msg = {'subject': subject, 'message': message}
+        self.queue.rpush('sender', json.dumps(msg))
+
+        print('Registered email!')
+
+    def send(self):
+        subject = request.forms.get('subject')
+        message = request.forms.get('message')
+
+        self.register_message(subject, message)
+
+        return 'Queued message! Subject: {}. Message: {}'.format(
+            subject,
+            message
+        )
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    sender = Sender()
+    sender.run(host='0.0.0.0', port=8080, debug=True)
---
+++ worker/worker.py
@@ -0,0 +1,14 @@
+import redis
+import json
+from time import sleep
+from random import randint
+
+if __name__ == '__main__':
+    r = redis.Redis(host='queue', port=6379, db=0)
+    while True:
+        message = json.loads(r.blpop('sender')[1])
+
+        # Simulate email send
+        print('Sending e-mail:', message['subject'])
+        sleep(randint(15, 45))
+        print('E-mail', message['subject'], 'sended')
