c71971093219f97829e09d999f16da2360969ea6
Igor José Castro Mariano Lima <igormarianolima05@gmail.com>
2026-01-04 20:13:05 -0300

    Chore: Scale worker to 3 instances

--- worker/worker.py
+++ worker/worker.py
@@ -5,6 +5,9 @@ from random import randint
 
 if __name__ == '__main__':
     r = redis.Redis(host='queue', port=6379, db=0)
+
+    print('Waiting e-mails...')
+
     while True:
         message = json.loads(r.blpop('sender')[1])
 
